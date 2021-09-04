c60931907fb44512c0db7856632aca9a0dc9f95b
Richmond Boahene <rkboahene2@gmail.com>
2021-09-04 23:30:21 +0000

    cart create function logic change 1

--- FashionShop1/cart/views.py
+++ FashionShop1/cart/views.py
@@ -8,14 +8,14 @@ def cart_create(user=None):
 
 def cart_home(request):
     cart_id = request.session.get('cart_id', None)
-    if cart_id is None:
+    # if cart_id is None:
+    #     cart_obj = cart_create()
+    #     request.session['cart_id'] = cart_obj.id
+    # else:
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
+        cart_obj = qs.first()
+    else:
         cart_obj = cart_create()
         request.session['cart_id'] = cart_obj.id
-    else:
-        qs = Cart.objects.filter(id=cart_id)
-        if qs.count() == 1:
-            cart_obj = qs.first()
-        else:
-            cart_obj = cart_create()
-            request.session['cart_id'] = cart_obj.id
     return render(request, "cart/home.html")
