f8e65ce47469d447d5aa38c9c1ed5890086e104e
Richmond Boahene <rkboahene2@gmail.com>
2021-09-06 08:29:16 +0000

    refactor cart view into model manager

--- FashionShop1/cart/views.py
+++ FashionShop1/cart/views.py
@@ -2,21 +2,18 @@ from django.shortcuts import render
 from .models import Cart
 # Create your views here.
 
-def cart_create(user=None):
-    cart_obj = Cart.objects.create(user=None)
-    return cart_obj
-
 
 def cart_home(request):
-    cart_id = request.session.get('cart_id', None)
+    # cart_id = request.session.get('cart_id', None)
     
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        cart_obj = qs.first()
-        if request.user.is_authenticated() and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.objects.new(user=request.user)
-        request.session['cart_id'] = cart_obj.id
+    # qs = Cart.objects.filter(id=cart_id)
+    # if qs.count() == 1:
+    #     cart_obj = qs.first()
+    #     if request.user.is_authenticated() and cart_obj.user is None:
+    #         cart_obj.user = request.user
+    #         cart_obj.save()
+    # else:
+    #     cart_obj = Cart.objects.new(user=request.user)
+    #     request.session['cart_id'] = cart_obj.id
+    cart_obj = Cart.objects.new_or_get(request)
     return render(request, "cart/home.html")
