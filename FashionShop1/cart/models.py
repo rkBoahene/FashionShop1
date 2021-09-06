ae9fb7606c70b26ff39d995411c44020057a6a26
Richmond Boahene <rkboahene2@gmail.com>
2021-09-06 08:17:40 +0000

    update cart functionality

--- FashionShop1/cart/models.py
+++ FashionShop1/cart/models.py
@@ -6,8 +6,12 @@ from products.models import Product
 User = settings.AUTH_USER_MODEL
 
 class CartManager(models.Manager):
-    def new_cart(self, user=None):
-        return self.model.objects.create(user=user)
+    def new(self, user=None):
+        user_obj = None
+        if user is not None:
+            if user.is_authenticated():
+                user_obj = user
+        return self.model.objects.create(user=user_obj)
 
 
 # Create your models here.
