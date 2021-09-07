649f35c42a6c98122b745470518c979f6557c240
Richmond Boahene <rkboahene2@gmail.com>
2021-09-07 21:33:11 +0000

    design cart detail page

--- FashionShop1/cart/views.py
+++ FashionShop1/cart/views.py
@@ -7,15 +7,19 @@ from products.models import Product
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
     
-    return render(request, "cart/home.html")
+    return render(request, "cart/home.html",{'cart':cart_obj})
 
 
 def cart_update(request):
-    product_id = 1
-    product_obj = Product.objects.get(id=product_id)
-    cart_obj, new_obj = Cart.objects.new_or_get(request)
-    if product_obj in cart_obj.products.all():
-        cart_obj.products.remove(product_obj)
-    else:
-        cart_obj.products.add(product_obj)
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        try:
+            product_obj = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            return redirect('cart:home')
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+        else:
+            cart_obj.products.add(product_obj)
     return redirect('cart:home')
