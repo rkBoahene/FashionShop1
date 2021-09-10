edf4b7ae0d0533902506fa8c58ba5f8cf4b3b507
Richmond Boahene <rkboahene2@gmail.com>
2021-09-10 11:04:31 +0000

    define initial order model

--- FashionShop1/orders/models.py
+++ FashionShop1/orders/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+from cart.models import Cart
+
+ORDER_STATUS_CHOICES = (
+    ('created','Created'),
+    ('paid','Paid'),
+    ('shipped','Shipped'),
+    ('refunded','Refunded')
+)
+
+
 # Create your models here.
+class Order(models.Model):
+    order_id = models.CharField( max_length=50, blank=True)
+    # billing_profile = 
+    # shipping_address =
+    # billing_address = 
+    cart = models.ForeignKey(Cart)
+    status = models.CharField(max_length=100, default='created', choices=ORDER_STATUS_CHOICES)
+    shipping_total = models.DecimalField(default=6.00 ,max_digits=10, decimal_places=2)
+    total = models.DecimalField(default=0.00 ,max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.order_id
+
+
+# generate order id
+# generate order total
