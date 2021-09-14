5b4a1dd406fbc6853be24bc955b8133c0e5cc331
Richmond Boahene <rkboahene2@gmail.com>
2021-09-14 11:40:36 +0000

    user account when checkout

--- FashionShop1/FashionShop1/settings.py
+++ FashionShop1/FashionShop1/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # my apps
     'accounts.apps.AccountsConfig',
+    'fashionUser.apps.FashionuserConfig',
     # 'shop.apps.ShopConfig',
     'products.apps.ProductsConfig',
     'search.apps.SearchConfig',
--- FashionShop1/cart/views.py
+++ FashionShop1/cart/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import redirect, render
 from .models import Cart
 from products.models import Product
 from orders.models import Order
+from accounts.forms import LoginForm
+from fashionUser.models import BillingProfile
 # Create your views here.
 
 
@@ -34,5 +36,16 @@ def checkout_home(request):
         return redirect('cart:home')
     else:
         order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
-    
-    return render(request, 'cart/checkout.html',{'object': order_obj})
+    user = request.user
+    billing_profile = None
+
+    login_form = LoginForm()
+    if user.is_authenticated:
+        billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(user=user, email=user.email)
+
+    context = {
+        'object': order_obj,
+        'billing_profile':billing_profile,
+        'login_form':login_form
+    }
+    return render(request, 'cart/checkout.html',context)
